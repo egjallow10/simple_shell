d8ecc02126fb64eae9cbdbb8e21952809f280856
egjallow10 <egjallow10@gmail.com>
2022-08-08 04:07:32 -0700

    added a struc to the shell.h

--- shell.h
+++ shell.h
@@ -15,3 +15,16 @@
 extern char **environ;
 
 extern int errno;
+
+/**
+  * struct built_in - struct for matching commands to built in commands
+  * @command: the string matching the command to execute
+  * @built_cmd: the built in command to call if command matches
+  * Description: Special built in commands for custom shell
+  */
+typedef struct built_in
+{
+	char *command;
+	char (*built_cmd)(char **, char **);
+} do_built;
+
