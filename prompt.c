79bfca8d3c64e69b8183779a90d97ae12939cb71
egjallow10 <egjallow10@gmail.com>
2022-08-08 03:42:19 -0700

    finding the list of argument

--- prompt.c
+++ prompt.c
@@ -16,3 +16,36 @@ int print_str(char *str)
 		i++;
 	return (write(STDOUT_FILENO, str, i));
 }
+
+/**
+  * builtin_finder - checks if the first argument matches a built in command
+  *
+  * @arglist: argument list from our shell
+  * Return: 0 if no match, 1 if exit, non 1/0 otherwise
+  */
+int builtin_finder(char **arglist)
+{
+	int i = 0, ret = -1, use = 0;
+	do_built built_commands[] = {
+		{"exit", exit_builtin},
+		{"env", env_builtin},
+		{NULL, NULL}
+	};
+
+	if (!arglist)
+		return (-1);
+
+	while (built_commands[i].command)
+	{
+		if (_strcmp(built_commands[i].command, arglist[use]))
+		{
+			i++;
+		}
+		else
+		{
+			ret = built_commands[i++].built_cmd(arglist, environ);
+			break;
+		}
+	}
+	return (ret);
+}
