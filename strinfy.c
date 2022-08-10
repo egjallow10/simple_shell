96e012bb7c5e47b12a865c914334355720cc273c
egjallow10 <egjallow10@gmail.com>
2022-08-10 00:45:03 -0700

    allocate memory

--- strinfy.c
+++ strinfy.c
@@ -29,3 +29,22 @@ char *_strcat_dir(char *dest, char *src)
 	return (cat);
 }
 
+/**
+  * _malloc - allocates memory for n chars and frees on failure
+  *
+  * @n: number of chars to allocate memory for
+  * @a: 2D array that string will be a part of
+  * Return: the pointer to chars
+  */
+char *_malloc(int n, char **a)
+{
+	char *ret;
+
+	ret = malloc(sizeof(char) * n);
+	if (!ret)
+	{
+		free_double(a);
+		return (NULL);
+	}
+	return (ret);
+}
